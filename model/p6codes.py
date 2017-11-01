54e1b3438f86c44a566e62c08501b14863ee502c
Hassan Emam <hassan.emam@hotmail.com>
2017-11-01 15:22:23 +0000

    Added Static Codes

--- model/p6codes.py
+++ model/p6codes.py
@@ -12,3 +12,21 @@ class ActivityType:
     Task = "TT_Task"
     StartMilestone = "TT_Mile"
     FinishMilestone = "TT_FinMile"
+    LevelOfEffort = "TT_LOE"
+    WBSSummary = "TT_WBS"
+
+
+class RelationshipType:
+
+    FF = "PR_FF"
+    FS = "PR_FS"
+    SS = "PR_SS"
+    SF = "PR_SF"
+
+
+class DurationTypes:
+
+    FixedQuantity = "DT_FixedQty"
+    FixedDurationAndUnits = "DT_FixedDrtn"
+    FixedRate = "DT_FixedRate"
+    FixedDuration = "DT_FixedDUR2"
