48f478fec2dc1981c4a5e06163524c068a2f007e
Osama Ata <370095+osama-ata@users.noreply.github.com>
2025-04-15 15:34:14 +0300

    Refactor and enhance reader.py for improved functionality and maintainability

--- xerparser/reader.py
+++ xerparser/reader.py
@@ -1,66 +1,136 @@
-# PyP6XER
-# Copyright (C) 2020, 2021 Hassan Emam <hassan@constology.com>
-#
-# This file is part of PyP6XER.
-#
-# PyP6XER library is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License v2.1 as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# PyP6XER is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with PyP6XER.  If not, see <https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html>.
-
-
-'''
+"""
 This file starts the process of reading and parsing xer files
 
-'''
+This module provides functionality to read and parse Primavera P6 XER files,
+transforming the tabular data into Python objects.
+"""
+
+import codecs
 import csv
 import mmap
-import codecs
-from xerparser import *
-from typing import List
+from typing import Any, Dict, List, Optional
+
+from xerparser.model.accounts import Accounts
+from xerparser.model.activitiyresources import ActivityResources
+from xerparser.model.activitycodes import ActivityCodes
+from xerparser.model.acttypes import ActTypes
+from xerparser.model.calendars import Calendars
+from xerparser.model.currencies import Currencies
+from xerparser.model.fintmpls import FinTmpls
+from xerparser.model.nonworks import NonWorks
+from xerparser.model.obss import OBSs
+from xerparser.model.pacttypes import PCatTypes
+from xerparser.model.pcatvals import PCatVals
+from xerparser.model.predecessors import Predecessors
+from xerparser.model.projcats import ProjCats
+from xerparser.model.projects import Projects
+from xerparser.model.rcattypes import RCatTypes
+from xerparser.model.rcatvals import RCatVals
+from xerparser.model.resources import Resources
+from xerparser.model.rolerates import RoleRates
+from xerparser.model.roles import Roles
+from xerparser.model.rsrccats import ResourceCategories
+from xerparser.model.rsrccurves import ResourceCurves
+from xerparser.model.rsrcrates import ResourceRates
+from xerparser.model.schedoptions import SchedOptions
+from xerparser.model.taskactvs import TaskActvs
+from xerparser.model.taskprocs import TaskProcs
+from xerparser.model.tasks import Tasks
+from xerparser.model.udftypes import UDFTypes
+from xerparser.model.udfvalues import UDFValues
+from xerparser.model.wbss import WBSs
 from xerparser.model.classes.data import Data
 from xerparser.write import writeXER
+from xerparser.model.classes.taskpred import TaskPred
+from xerparser.model.classes.projcat import ProjCat
+from xerparser.model.classes.taskproc import TaskProc
+from xerparser.model.classes.fintmpl import FinTmpl
+from xerparser.model.classes.nonwork import NonWork
+
 
 class Reader:
-
     """
-    Reader class is the main parser for xer files. It takes a file path as a parameter.
-    The read records are parsed into classes depending on their types like activities, wbs,etc.
+    Main parser class for Primavera P6 XER files.
 
-    :param <file>: is an xer file that need to be parsed
-    :return: Reader object that contains collection of parsed records
-    :Example:
+    This class reads an XER file and parses its contents into various Python objects
+    representing projects, tasks, resources, and other Primavera P6 entities.
 
-    from xerparser.reader import Reader
-    file = Reader('file.xer')
+    Parameters
+    ----------
 
+    filename : str
+        Path to the XER file to be parsed
+
+    Attributes
+    ----------
+
+    file : str
+        Path to the XER file
+    projects : Projects
+        Collection of projects in the XER file
+    activities : Tasks
+        Collection of activities/tasks in the XER file
+    wbss : WBSs
+        Collection of Work Breakdown Structure elements in the XER file
+    relations : Predecessors
+        Collection of relationships between activities in the XER file
+
+    Examples
+    --------
+
+    >>> from xerparser.reader import Reader
+    >>> xer = Reader("myproject.xer")
+    >>> for project in xer.projects:
+    ...     print(project)
     """
 
-    current_table = ''
-    current_headers = []
+    current_table: str = ""
+    current_headers: List[str] = []
 
-    def write(self, filename=None):
+    def write(self, filename: Optional[str] = None) -> None:
+        """
+        Write the parsed data back to an XER file.
+
+        Parameters
+        ----------
+
+        filename : str, optional
+            Path to the output XER file. If None, an exception is raised.
+
+        Raises
+        ------
+
+        Exception
+            If filename is None
+
+        Returns
+        -------
+
+        None
+        """
         if filename is None:
-            raise Exception("You have to provide the filename")
+            raise ValueError("You have to provide the filename")
         writeXER(self, filename)
 
-    def create_object(self, object_type, params):
+    def create_object(self, object_type: str, params: Dict[str, Any]) -> None:
         """
+        Create appropriate objects based on the record type.
 
-        Args:
-            object_type:
-            params:
+        This method creates and adds objects to their respective collections
+        based on the type of record found in the XER file.
 
-        Returns:
+        Parameters
+        ----------
 
+        object_type : str
+            The type of record in the XER file
+        params : dict
+            The parameters parsed from the record
+
+        Returns
+        -------
+
+        None
         """
         if object_type.strip() == "CURRTYPE":
             self._currencies.add(params)
@@ -121,164 +191,398 @@ class Reader:
         elif object_type.strip() == "NONWORK":
             self._nonworks.add(params)
 
-    def summary(self):
-        print('Number of activities: ', self.tasks.count)
-        print('Number of relationships: ', len(TaskPred.obj_list))
+    def summary(self) -> None:
+        """
+        Print a summary of the parsed XER file.
+
+        Displays the number of activities and relationships in the parsed file.
+
+        Returns
+        -------
+
+        None
+        """
+        print("Number of activities: ", self.activities.count)
+        print("Number of relationships: ", len(TaskPred.obj_list))
 
     @property
     def projects(self) -> Projects:
         """
+        Get all projects in the XER file.
+
+        Returns
+        -------
+
         Projects
-
-        :return: list of all projects contained in the xer file
-
-        todo:: text
-
+            Collection of all projects contained in the XER file
         """
         return self._projects
 
     @property
     def activities(self) -> Tasks:
         """
-        Property to retrieve list of tasks
-        Returns: list of tasks
+        Get all tasks/activities in the XER file.
+
+        Returns
+        -------
+
+        Tasks
+            Collection of all tasks contained in the XER file
         """
         return self._tasks
 
     @property
     def wbss(self) -> WBSs:
         """
-        Property to return all wbs elements in the parsed file
-        Returns:
-            list of wbs elements
+        Get all Work Breakdown Structure elements in the XER file.
+
+        Returns
+        -------
+
+        WBSs
+            Collection of all WBS elements in the XER file
         """
         return self._wbss
 
     @property
     def relations(self) -> Predecessors:
         """
-        Property to retrieve relationships from parsed file
-        Returns:
-            list of relations
+        Get all relationships between activities in the XER file.
+
+        Returns
+        -------
+
+        Predecessors
+            Collection of all relationships in the XER file
         """
         return self._predecessors
 
     @property
     def resources(self) -> Resources:
         """
-        Property to return a list of resources from parsed file
-        Returns:
-            list of resources of type Resources
+        Get all resources in the XER file.
+
+        Returns
+        -------
+
+        Resources
+            Collection of all resources in the XER file
         """
         return self._resources
 
     @property
     def accounts(self) -> Accounts:
         """
-        Property to return a list of accounts from parsed files
-        Returns:
-            list of accounts of type Accounts
+        Get all accounts in the XER file.
+
+        Returns
+        -------
+
+        Accounts
+            Collection of all accounts in the XER file
         """
         return self._accounts
 
     @property
     def activitycodes(self) -> ActivityCodes:
         """
-        Property to return a list of activity codes from parsed files
-        Returns:
-            list of accounts of type ActivityCode
+        Get all activity codes in the XER file.
 
+        Returns
+        -------
+
+        ActivityCodes
+            Collection of all activity codes in the XER file
         """
         return self._activitycodes
+
     @property
     def actvcodes(self) -> TaskActvs:
+        """
+        Get all activity code values in the XER file.
+
+        Returns
+        -------
+
+        TaskActvs
+            Collection of all activity code values in the XER file
+        """
         return self._actvcodes
 
     @property
     def acttypes(self) -> ActTypes:
+        """
+        Get all activity types in the XER file.
+
+        Returns
+        -------
+
+        ActTypes
+            Collection of all activity types in the XER file
+        """
         return self._acttypes
 
     @property
     def calendars(self) -> Calendars:
+        """
+        Get all calendars in the XER file.
+
+        Returns
+        -------
+
+        Calendars
+            Collection of all calendars in the XER file
+        """
         return self._calendars
 
     @property
     def currencies(self) -> Currencies:
+        """
+        Get all currencies in the XER file.
+
+        Returns
+        -------
+
+        Currencies
+            Collection of all currencies in the XER file
+        """
         return self._currencies
 
     @property
     def obss(self) -> OBSs:
+        """
+        Get all OBS elements in the XER file.
+
+        Returns
+        -------
+
+        OBSs
+            Collection of all OBS elements in the XER file
+        """
         return self._obss
 
     @property
     def rcattypes(self) -> RCatTypes:
+        """
+        Get all resource category types in the XER file.
+
+        Returns
+        -------
+
+        RCatTypes
+            Collection of all resource category types in the XER file
+        """
         return self._rcattypes
 
     @property
     def rcatvals(self) -> RCatVals:
+        """
+        Get all resource category values in the XER file.
+
+        Returns
+        -------
+
+        RCatVals
+            Collection of all resource category values in the XER file
+        """
         return self._rcatvals
 
     @property
     def rolerates(self) -> RoleRates:
+        """
+        Get all role rates in the XER file.
+
+        Returns
+        -------
+
+        RoleRates
+            Collection of all role rates in the XER file
+        """
         return self._rolerates
 
     @property
     def roles(self) -> Roles:
+        """
+        Get all roles in the XER file.
+
+        Returns
+        -------
+
+        Roles
+            Collection of all roles in the XER file
+        """
         return self._roles
 
     @property
     def resourcecurves(self) -> ResourceCurves:
+        """
+        Get all resource curves in the XER file.
+
+        Returns
+        -------
+
+        ResourceCurves
+            Collection of all resource curves in the XER file
+        """
         return self._rsrcurves
 
     @property
     def resourcerates(self) -> ResourceRates:
+        """
+        Get all resource rates in the XER file.
+
+        Returns
+        -------
+
+        ResourceRates
+            Collection of all resource rates in the XER file
+        """
         return self._rsrcrates
 
     @property
     def resourcecategories(self) -> ResourceCategories:
+        """
+        Get all resource categories in the XER file.
+
+        Returns
+        -------
+
+        ResourceCategories
+            Collection of all resource categories in the XER file
+        """
         return self._rsrccats
 
     @property
     def scheduleoptions(self) -> SchedOptions:
+        """
+        Get all schedule options in the XER file.
+
+        Returns
+        -------
+
+        SchedOptions
+            Collection of all schedule options in the XER file
+        """
         return self._schedoptions
 
     @property
     def activityresources(self) -> ActivityResources:
+        """
+        Get all activity resources in the XER file.
+
+        Returns
+        -------
+
+        ActivityResources
+            Collection of all activity resources in the XER file
+        """
         return self._activityresources
 
     @property
     def udfvalues(self) -> UDFValues:
+        """
+        Get all UDF values in the XER file.
+
+        Returns
+        -------
+
+        UDFValues
+            Collection of all UDF values in the XER file
+        """
         return self._udfvalues
 
     @property
     def udftypes(self) -> UDFTypes:
+        """
+        Get all UDF types in the XER file.
+
+        Returns
+        -------
+
+        UDFTypes
+            Collection of all UDF types in the XER file
+        """
         return self._udftypes
 
     @property
     def pcattypes(self) -> List[PCatTypes]:
+        """
+        Get all project category types in the XER file.
+
+        Returns
+        -------
+
+        List[PCatTypes]
+            Collection of all project category types in the XER file
+        """
         return self._pcattypes
 
     @property
     def pcatvals(self) -> List[PCatVals]:
+        """
+        Get all project category values in the XER file.
+
+        Returns
+        -------
+
+        List[PCatVals]
+            Collection of all project category values in the XER file
+        """
         return self._pcatvals
 
     @property
     def projpcats(self) -> List[ProjCat]:
+        """
+        Get all project categories in the XER file.
+
+        Returns
+        -------
+
+        List[ProjCat]
+            Collection of all project categories in the XER file
+        """
         return self._projpcats
 
     @property
     def taskprocs(self) -> List[TaskProc]:
+        """
+        Get all task procedures in the XER file.
+
+        Returns
+        -------
+
+        List[TaskProc]
+            Collection of all task procedures in the XER file
+        """
         return self._taskprocs
 
     @property
     def fintmpls(self) -> List[FinTmpl]:
+        """
+        Get all financial templates in the XER file.
+
+        Returns
+        -------
+
+        List[FinTmpl]
+            Collection of all financial templates in the XER file
+        """
         return self._fintmpls
 
     @property
     def nonworks(self) -> List[NonWork]:
+        """
+        Get all non-work periods in the XER file.
+
+        Returns
+        -------
+
+        List[NonWork]
+            Collection of all non-work periods in the XER file
+        """
         return self._nonworks
 
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.file = filename
         self._tasks = Tasks()
         self._predecessors = Predecessors()
@@ -317,10 +621,10 @@ class Reader:
         self._data.taskresource = self._activityresources
         self._data.taskactvcodes = self._activitycodes
         self._data.predecessors = self._predecessors
-        with codecs.open(filename, encoding='utf-8', errors='ignore') as tsvfile:
-            stream = csv.reader(tsvfile, delimiter='\t')
+        with codecs.open(filename, encoding="utf-8", errors="ignore") as tsvfile:
+            stream = csv.reader(tsvfile, delimiter="\t")
             for row in stream:
-                if row[0] =="%T":
+                if row[0] == "%T":
                     current_table = row[1]
                 elif row[0] == "%F":
                     current_headers = [r.strip() for r in row[1:]]
@@ -335,10 +639,25 @@ class Reader:
         #     elif line_lst[0] == "%R":
         #         self.create_object(current_table, line_lst[1:])
 
-    def get_num_lines(self, file_path):
-        fp = open(file_path, "r+")
-        buf = mmap.mmap(fp.fileno(), 0)
-        lines = 0
-        while buf.readline():
-            lines += 1
+    def get_num_lines(self, file_path: str) -> int:
+        """
+        Get the number of lines in a file.
+
+        Parameters
+        ----------
+
+        file_path : str
+            Path to the file
+
+        Returns
+        -------
+
+        int
+            Number of lines in the file
+        """
+        with open(file_path, "r+", encoding="utf-8", errors="ignore") as fp:
+            buf = mmap.mmap(fp.fileno(), 0)
+            lines = 0
+            while buf.readline():
+                lines += 1
         return lines
