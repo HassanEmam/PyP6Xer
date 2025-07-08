aa7b225ad0fa9c8da4ac640b2d589456ec0fe321
Liam McDevitt <liam.mcdevitt@trunk.tools>
2025-07-08 10:39:23 -0400

    Making Safer Types

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup
 
+
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
-setup(
-    name='PyP6Xer',
-    version='1.015.00',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-    packages=['xerparser', 'xerparser.dcma14', 'xerparser.model', 'xerparser.model.classes'],
-    url='',
-    license='GNU GENERAL PUBLIC LICENSE',
-    author='Hassan',
-    author_email='hassan.emam@hotmail.com',
-    install_requires=[
 
-      ],
-    description='Parser for XER written in Python'
+setup(
+    name="PyP6Xer_SafeTypes",
+    version="1.015.00",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=[
+        "xerparser",
+        "xerparser.dcma14",
+        "xerparser.model",
+        "xerparser.model.classes",
+    ],
+    url="",
+    license="GNU GENERAL PUBLIC LICENSE",
+    author="Hassan",
+    author_email="hassan.emam@hotmail.com",
+    install_requires=[],
+    description="Parser for XER written in Python with safe types",
 )
--- xerparser/model/classes/taskpred.py
+++ xerparser/model/classes/taskpred.py
@@ -16,32 +16,86 @@
 # You should have received a copy of the GNU General Public License
 # along with PyP6XER.  If not, see <https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html>.
 
+import locale
+
+
+def safe_atoi(val: str | None) -> int:
+    """Convert string to integer, handling null/empty values and converting floats to int"""
+    if val is None:
+        return 0
+    val = val.strip().lower()
+    if val in ("", "null", "none"):
+        return 0
+    try:
+        # Handle both integer and float strings
+        float_val = locale.atof(val)
+        return int(float_val)
+    except ValueError:
+        return 0
+
 
 class TaskPred:
-
     obj_list = []
 
     def __init__(self, params):
-        self.task_pred_id = params.get('task_pred_id').strip() if params.get('task_pred_id') else None
-        self.task_id = int(params.get('task_id')) if params.get('task_id') else None
-        self.pred_task_id = int(params.get('pred_task_id')) if params.get('pred_task_id') else None
-        self.proj_id = int(params.get('proj_id').strip()) if params.get('proj_id') else None
-        self.pred_proj_id = int(params.get('proj_id').strip()) if params.get('pred_proj_id') else None
-        self.pred_type = params.get('pred_type').strip() if params.get('pred_type') else None
-        self.lag_hr_cnt = float(params.get('lag_hr_cnt').strip()) if params.get('lag_hr_cnt') else None
-        self.float_path = params.get('float_path').strip() if params.get('float_path') else None
-        self.aref = params.get('aref').strip() if params.get('aref') else None
-        self.arls = params.get('arls').strip() if params.get('arls') else None
-        self.comments = params.get('comments').strip() if params.get('comments') else None
+        self.task_pred_id = (
+            params.get("task_pred_id").strip() if params.get("task_pred_id") else None
+        )
+        self.task_id = int(params.get("task_id")) if params.get("task_id") else None
+        self.pred_task_id = (
+            int(params.get("pred_task_id")) if params.get("pred_task_id") else None
+        )
+        self.proj_id = (
+            int(params.get("proj_id").strip()) if params.get("proj_id") else None
+        )
+        self.pred_proj_id = (
+            int(params.get("proj_id").strip()) if params.get("pred_proj_id") else None
+        )
+        self.pred_type = (
+            params.get("pred_type").strip() if params.get("pred_type") else None
+        )
+        self.lag_hr_cnt = (
+            safe_atoi(params.get("lag_hr_cnt").strip())
+            if params.get("lag_hr_cnt")
+            else None
+        )
+        self.float_path = (
+            params.get("float_path").strip() if params.get("float_path") else None
+        )
+        self.aref = params.get("aref").strip() if params.get("aref") else None
+        self.arls = params.get("arls").strip() if params.get("arls") else None
+        self.comments = (
+            params.get("comments").strip() if params.get("comments") else None
+        )
         TaskPred.obj_list.append(self)
 
     def get_id(self):
         return self.task_pred_id
 
     def get_tsv(self):
-        tsv = ['%R', self.task_pred_id, self.task_id, self.pred_task_id, self.proj_id, self.pred_proj_id,
-               self.pred_type, self.lag_hr_cnt, self.comments, self.float_path, self.aref, self.arls]
+        tsv = [
+            "%R",
+            self.task_pred_id,
+            self.task_id,
+            self.pred_task_id,
+            self.proj_id,
+            self.pred_proj_id,
+            self.pred_type,
+            self.lag_hr_cnt,
+            self.comments,
+            self.float_path,
+            self.aref,
+            self.arls,
+        ]
         return tsv
-    
+
     def __repr__(self):
-        return str(self.pred_task_id) + '- ' + self.pred_type + ' ->' + str(self.task_id) + ' lag: ' + str(self.lag_hr_cnt)
+        return (
+            str(self.pred_task_id)
+            + "- "
+            + self.pred_type
+            + " ->"
+            + str(self.task_id)
+            + " lag: "
+            + str(self.lag_hr_cnt)
+        )
